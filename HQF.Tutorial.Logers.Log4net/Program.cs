dfa60cb1e680f3e733a518be50b0f0060c0f9f04
huoxudong125 <423huo@163.com>
2016-01-15 18:23:04 +0800

    Add Two Logger,Need to implement

---
+++ HQF.Tutorial.Logers.Log4net/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using log4net;
+
+namespace HQF.Tutorial.Logers.Log4net
+{
+    //注意下面的语句一定要加上，指定log4net使用.config文件来读取配置信息
+    //如果是WinForm（假定程序为MyDemo.exe，则需要一个MyDemo.exe.config文件）
+    //如果是WebForm，则从web.config中读取相关信息
+    //[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 1; i++)
+            {
+                //创建日志记录组件实例  
+                //ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);  
+
+                ILog log = log4net.LogManager.GetLogger(typeof (Program));
+                //记录错误日志  
+                //log.Error("error", new Exception("在这里发生了一个异常,Error Number:"+random.Next()));  
+                //记录严重错误  
+                //log.Fatal("fatal", new Exception("在发生了一个致命错误，Exception Id："+random.Next()));  
+                //记录一般信息  
+                //log.Info("提示：系统正在运行");  
+                //记录调试信息  
+                //log.Debug("调试信息：debug");  
+                //记录警告信息  
+                //log.Warn("警告：warn");  
+            }
+            Console.WriteLine("日志记录完毕。");
+            Console.Read();
+
+        }
+    }
+} 
---
+++ HQF.Tutorial.Logers.NLog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQF.Tutorial.Logers.NLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

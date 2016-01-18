321a5c35210d8dd48b853f99e1a4fcc88355b172
huoxudong125 <423huo@163.com>
2016-01-18 09:35:53 +0800

    Fix make all code work ,for log4net

--- HQF.Tutorial.Logers.Log4net/Program.cs
+++ HQF.Tutorial.Logers.Log4net/Program.cs
@@ -19,16 +19,16 @@ namespace HQF.Tutorial.Logers.Log4net
                 //ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);  
 
                 ILog log = log4net.LogManager.GetLogger(typeof (Program));
-                //记录错误日志  
-                //log.Error("error", new Exception("在这里发生了一个异常,Error Number:"+random.Next()));  
-                //记录严重错误  
-                //log.Fatal("fatal", new Exception("在发生了一个致命错误，Exception Id："+random.Next()));  
-                //记录一般信息  
-                //log.Info("提示：系统正在运行");  
-                //记录调试信息  
-                //log.Debug("调试信息：debug");  
-                //记录警告信息  
-                //log.Warn("警告：warn");  
+                //记录错误日志
+                log.Error("error", new Exception("在这里发生了一个异常,Error Number:" + random.Next()));
+                //记录严重错误
+                log.Fatal("fatal", new Exception("在发生了一个致命错误，Exception Id：" + random.Next()));
+                //记录一般信息
+                log.Info("提示：系统正在运行");
+                //记录调试信息
+                log.Debug("调试信息：debug");
+                //记录警告信息
+                log.Warn("警告：warn");
             }
             Console.WriteLine("日志记录完毕。");
             Console.Read();

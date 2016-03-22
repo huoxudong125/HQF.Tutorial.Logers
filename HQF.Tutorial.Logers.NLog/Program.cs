11a9213f2516ca78b245a55078497c05ac33f403
huoxudong125 <423huo@163.com>
2016-03-22 13:54:18 +0800

    Enable configuaration watch in configFile.

--- HQF.Tutorial.Logers.NLog/Program.cs
+++ HQF.Tutorial.Logers.NLog/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace HQF.Tutorial.Logers.NLog
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method 
+            // and pass log level as the parameter.
+            logger.Log(LogLevel.Info, "Sample informational message");
+
+            Console.ReadKey();
+
         }
     }
 }
